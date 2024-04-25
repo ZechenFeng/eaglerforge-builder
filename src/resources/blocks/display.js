2ed9606ac60c700580ece1144268b575278bee14
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-04-25 17:42:04 +0330

    Update display.js

--- src/resources/blocks/display.js
+++ src/resources/blocks/display.js
@@ -49,6 +49,96 @@ function register() {
     }, (block) => {
         return [`ModAPI.getdisplayWidth()`, javascriptGenerator.ORDER_ATOMIC];
     })
+    registerBlock(`${categoryPrefix}drawstring`, {
+        message0: 'draw %1 to x: %2 and y: %3 on screen with color %4',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "TEXT1"
+            }
+        ],
+        args1: [
+            {
+                "type": "input_value",
+                "name": "TEXT2"
+            }
+        ],
+        args2: [
+            {
+                "type": "input_value",
+                "name": "TEXT3"
+            }
+        ],
+        args3: [
+            {
+                "type": "input_value",
+                "name": "TEXT4"
+            }
+        ],
+        args4: [
+            {
+                "type": "input_value",
+                "name": "TEXT5"
+            }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor,
+    }, (block) => {
+        const TEXT1 = javascriptGenerator.valueToCode(block, 'TEXT1', javascriptGenerator.ORDER_ATOMIC);
+        const TEXT2 = javascriptGenerator.valueToCode(block, 'TEXT1', javascriptGenerator.ORDER_ATOMIC);
+        const TEXT3 = javascriptGenerator.valueToCode(block, 'TEXT1', javascriptGenerator.ORDER_ATOMIC);
+        const TEXT4 = javascriptGenerator.valueToCode(block, 'TEXT1', javascriptGenerator.ORDER_ATOMIC);
+        const TEXT5 = javascriptGenerator.valueToCode(block, 'TEXT1', javascriptGenerator.ORDER_ATOMIC);
+        const code = `ModAPI.drawString({msg: ${TEXT1}, x: ${TEXT2}, y: ${TEXT3}, color: ${TEXT4}});`;
+        return `${code}\n`;
+    })
+    registerBlock(`${categoryPrefix}drawstringwithshadow`, {
+        message0: 'draw %1 to x: %2 and y: %3 on screen with color %4 with shadow',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "TEXT1"
+            }
+        ],
+        args1: [
+            {
+                "type": "input_value",
+                "name": "TEXT2"
+            }
+        ],
+        args2: [
+            {
+                "type": "input_value",
+                "name": "TEXT3"
+            }
+        ],
+        args3: [
+            {
+                "type": "input_value",
+                "name": "TEXT4"
+            }
+        ],
+        args4: [
+            {
+                "type": "input_value",
+                "name": "TEXT5"
+            }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor,
+    }, (block) => {
+        const TEXT1 = javascriptGenerator.valueToCode(block, 'TEXT1', javascriptGenerator.ORDER_ATOMIC);
+        const TEXT2 = javascriptGenerator.valueToCode(block, 'TEXT1', javascriptGenerator.ORDER_ATOMIC);
+        const TEXT3 = javascriptGenerator.valueToCode(block, 'TEXT1', javascriptGenerator.ORDER_ATOMIC);
+        const TEXT4 = javascriptGenerator.valueToCode(block, 'TEXT1', javascriptGenerator.ORDER_ATOMIC);
+        const TEXT5 = javascriptGenerator.valueToCode(block, 'TEXT1', javascriptGenerator.ORDER_ATOMIC);
+        const code = `ModAPI.drawStringWithShadow({msg: ${TEXT1}, x: ${TEXT2}, y: ${TEXT3}, color: ${TEXT4}});`;
+        return `${code}\n`;
+    })
 }
 
 export default register;
